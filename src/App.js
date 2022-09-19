82d0b758fcf952f191883942fef34eee53b4d7cc
Chen Kel Vin <vnbkelvin@gmail.com>
2022-09-19 10:12:46 +0800

    add title for home

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Kelvin's customised site</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
